d38d3ad56aa027b0e5c824437f6ce7c772454798
luhFraud <spartida0002@gmail.com>
2023-01-06 21:17:31 -0800

    added winning logic

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const hover = document.querySelectorAll('.box');
 
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
 // Game board module
 const gameBoard = (() => {
-    const board = ['','','','','','','','',''];
+    const board = [];
     return {board};
 })();
-console.log(gameBoard.board);
+
 // Factory Function: Creates players
 const player = (name, symbol) => {
     const getName = () => name;
@@ -18,15 +29,20 @@ const player = (name, symbol) => {
             currentTurn = player1
         }
     }
-
-    const storeSymbol = () => {
-        gameBoard.board.push(currentTurn.getSymbol());
-    };
-
+    
+    const checkWinner = () => {
+        winningConditions.forEach((item) => {
+            if (gameBoard.board[item[0]] === currentTurn.getSymbol() && 
+            gameBoard.board[item[1]] === currentTurn.getSymbol() && 
+            gameBoard.board[item[2]] === currentTurn.getSymbol()) {
+                console.log('winner' , currentTurn.getSymbol())
+            }
+        })
+    }
     
 
 
-    return {getName, getSymbol, changeTurns, storeSymbol}
+    return {getName, getSymbol, changeTurns, checkWinner}
 };
 
 const player1 = player('Player 1', 'X');
@@ -41,9 +57,44 @@ for (let i = 0; i < hover.length; i++ ) {
 
     function showSymbol() {
         hover[i].textContent = currentTurn.getSymbol();
-        //currentTurn.storeSymbol();
-        console.log(`${hover[i].id}`)
+        storeSymbol();
+        currentTurn.checkWinner();
         currentTurn.changeTurns();
+    }
+
+    
+    function storeSymbol() {
+        
+        if(`${hover[i].id}` === '0'){
+            gameBoard.board[0] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[0])
+        } else if(`${hover[i].id}` === '1') {
+            gameBoard.board[1] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[1])
+        } else if(`${hover[i].id}` === '2') {
+            gameBoard.board[2] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[2])
+        } else if(`${hover[i].id}` === '3') {
+            gameBoard.board[3] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[3])
+        } else if(`${hover[i].id}` === '4') {
+            gameBoard.board[4] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[4])
+        } else if(`${hover[i].id}` === '5') {
+            gameBoard.board[5] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[5])
+        } else if(`${hover[i].id}` === '6') {
+            gameBoard.board[6] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[6])
+        } else if(`${hover[i].id}` === '7') {
+            gameBoard.board[7] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[7])
+        } else {
+            gameBoard.board[8] = (currentTurn.getSymbol());
+            console.log(gameBoard.board[8])
+        }
+
+        console.log(gameBoard.board);
     }
     
 
@@ -68,7 +119,7 @@ for (let i = 0; i < hover.length; i++ ) {
         } else {
             hover[i].classList.toggle('hover8');
         }
-        console.log(hover[i]);
+        
         
     })
     hover[i].addEventListener('mouseleave', function() {
